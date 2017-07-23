3d53f607305aa222aef1ce346a2309e6ab2c7600
behzad00 <behzadpouyanfar@gmail.com>
2017-07-23 00:21:22 -0400

    got utils from James file

---
+++ app/components/utils/tracks.js
@@ -0,0 +1,35 @@
+
+import React, { Component } from 'react';
+import axios from 'axios';
+
+
+let songs =[];
+let titles=[];
+
+export default function songsToGuess(){
+
+
+
+	return axios.get("https://api.napster.com/v2.1/tracks/top?apikey=ZTk2YjY4MjMtMDAzYy00MTg4LWE2MjYtZDIzNjJmMmM0YTdm")
+
+	.then(function(response){
+
+
+		for(var i = 0; i<response.data.tracks.length; i++){
+
+			songs.push(response.data.tracks[i].previewURL);
+			titles.push(response.data.tracks[i].name);
+		}
+
+		//console.log(songs);
+		//console.log(titles);
+
+
+	})
+	.catch((error) =>reject(error));
+
+
+
+
+
+	}
