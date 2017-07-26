689b2ea70f1d2459721291592e7f3aa41828eafb
behzad00 <behzadpouyanfar@gmail.com>
2017-07-26 13:02:18 -0400

    array of mock data

--- app/components/Scoreboard.js
+++ app/components/Scoreboard.js
@@ -3,10 +3,30 @@ import React from 'react';
 class Scoreboard extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {
-            teamName: "Hello",
-            score: 10
-        };
+       this.state = {
+       scores: [
+                {
+                    teamName: 'team1',
+                    score: 16,
+                },
+                {
+                    teamName: 'team2',
+                    score: 18,
+                },
+                {
+                    teamName: 'team3',
+                    score: 12,
+                },
+                {
+                    teamName: 'team4',
+                    score: 10,
+                },
+                {
+                    teamName: 'team5',
+                    score: 14,
+                }
+            ]
+        }
     }
 
     render () {
